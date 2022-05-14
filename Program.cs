02dba097766c0ce6eed2bd2ad79fd2455e973da9
Giovanna Casalunga <100002176+GCasalunga@users.noreply.github.com>
2022-05-13 22:26:03 -0300

    git commit -m "Tarefa Finalizada"

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int contador = 0; 
+Console.Write("Quantas vezes?  ");
+int qtdvezes = Convert.ToInt32(Console.ReadLine());
+
+while (contador < qtdvezes)
+
+{
+
+contador = contador + 1;
+Console.WriteLine($"{contador}x Essa é a frase.");
+
+}
